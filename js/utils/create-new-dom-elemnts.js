c74527f188b2aa00795b33b55841f1ea27412671
Denis <waridesu@gmail.com>
2021-06-28 14:58:25 +0300

    friend assist:
    - To the template string

--- js/utils/create-new-dom-elemnts.js
+++ js/utils/create-new-dom-elemnts.js
@@ -1,55 +1,25 @@
-
-const cardTemplate = document.querySelector('#card').content;
-
 function createNewDomElement(item) {
-  const cardAvatar = cardTemplate.querySelector('.popup__avatar');
-  const cardTitle = cardTemplate.querySelector('.popup__title');
-  const cardAddress = cardTemplate.querySelector('.popup__text--address');
-  const cardPrice = cardTemplate.querySelector('.popup__text--price');
-  const cardType = cardTemplate.querySelector('.popup__type');
-  const cardGuestsNumber = cardTemplate.querySelector('.popup__text--capacity');
-  const cardInOutTime = cardTemplate.querySelector('.popup__text--time');
-  const cardFeatures = cardTemplate.querySelector('.popup__features');
-  const cardDescription = cardTemplate.querySelector('.popup__description');
-  const cardPhotos = cardTemplate.querySelector('.popup__photos');
-
-  function removeChild(element) {
-    for (let index = element.children.length - 1; index >= 0; index--) {
-      const child = element.children[index];
-      child.parentElement.removeChild(child);
-    }
+  function hideIfNull(params) {
+    return !params ? 'hidden' : '';
   }
 
-  function hideIfNull(params, element) {
-    if (!params) {
-      element.classList.add('hidden');
-    }
-  }
-
-  cardAvatar.src = item.author.avatar;
-  hideIfNull(item.author.avatar, cardAvatar);
-  cardTitle.innerText = item.offer.title;
-  hideIfNull(item.offer.title, cardTitle);
-  cardAddress.innerText = item.offer.address;
-  hideIfNull(item.offer.address, cardAddress);
-  cardPrice.innerText = `${item.offer.price} ₽/ночь`;
-  hideIfNull(item.offer.price, cardPrice);
-  cardType.innerText = item.offer.type;
-  hideIfNull(item.offer.type, cardType);
-  cardGuestsNumber.innerText = `${item.offer.rooms} комнаты для ${item.offer.guests} гостей`;
-  hideIfNull((item.offer.rooms && item.offer.guests), cardGuestsNumber);
-  cardInOutTime.innerText = `${item.offer.checkin}, выезд до ${item.offer.checkout}`;
-  hideIfNull((item.offer.checkin && item.offer.checkout), cardInOutTime);
-  removeChild(cardFeatures);
-  item.offer.features.forEach((element) => cardFeatures.innerHTML +=
-    `<li class="popup__feature popup__feature--${element}"></li>`);
-  hideIfNull(item.offer.features, cardFeatures);
-  cardDescription.innerText = item.offer.description;
-  hideIfNull(item.offer.description, cardDescription);
-  removeChild(cardPhotos);
-  item.offer.photos.forEach((element) => cardPhotos.innerHTML +=
-    `<img src="${element}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`);
-  hideIfNull(item.offer.photos, cardPhotos);
+  const cardTemplate =
+    `<article class="popup">
+      <img src="${item.author.avatar}" class="popup__avatar ${hideIfNull(item.author.avatar)}" width="70" height="70" alt="Аватар пользователя">
+      <h3 class="popup__title ${hideIfNull(item.offer.title)}">${item.offer.title}</h3>
+      <p class="popup__text popup__text--address ${hideIfNull(item.offer.address)}">${item.offer.address}</p>
+      <p class="popup__text popup__text--price ${hideIfNull(item.offer.price)}">${item.offer.price} <span>₽/ночь</span></p>
+      <h4 class="popup__type ${hideIfNull(item.offer.type)}">${item.offer.type}</h4>
+      <p class="popup__text popup__text--capacity ${hideIfNull(item.offer.rooms && item.offer.guests)}">${item.offer.rooms} комнаты для ${item.offer.guests} гостей</p>
+      <p class="popup__text popup__text--time ${hideIfNull(item.offer.checkin && item.offer.checkout)}">${item.offer.checkin}, выезд до ${item.offer.checkout}</p>
+      <ul class="popup__features ${hideIfNull(item.offer.features)}">
+        ${item.offer.features.forEach((element) => cardTemplate.querySelector('.popup__features').append(`<li className="popup__feature popup__feature--${element}"></li>`))}
+      </ul>
+      <p class="popup__description ${hideIfNull(item.offer.description)}">${item.offer.description}</p>
+      <div class="popup__photos ${hideIfNull(item.offer.photos)}">
+        ${item.offer.photos.forEach((element) => cardTemplate.querySelector('.popup__photos').append(`<img src="${element}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`))}
+      </div>
+    </article>`;
   return cardTemplate;
 }
 
