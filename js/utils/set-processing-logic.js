58c2447b274353cb3be38a86e54e8b55b6d11de5
Denis <waridesu@gmail.com>
2021-07-02 20:45:16 +0300

    true or false:
    - residence type and price
    - synchronize timeIn and timeOut

--- js/utils/set-processing-logic.js
+++ js/utils/set-processing-logic.js
@@ -1,5 +1,8 @@
 const typeOfResidence = document.querySelector('#type');
-
+if(typeOfResidence.value === 'bungalow'){
+  const priceByNight = document.querySelector('#price');
+  priceByNight.value = 0;
+}
 typeOfResidence.addEventListener('change', (event) => {
   const priceByNight = document.querySelector('#price');
   switch(event.target.value) {
