6c5f41f2cf027b83c48c7a23404ea7dede3af963
Denis <waridesu@gmail.com>
2021-07-23 17:01:56 +0300

    code review

--- js/utils/set-processing-logic.js
+++ js/utils/set-processing-logic.js
@@ -26,10 +26,46 @@ const typeOfResidenceHandler = (event) => {
 typeOfResidence.addEventListener('change', typeOfResidenceHandler);
 
 const time = document.querySelector('.ad-form__element--time');
-const timeHandler = (event)=> {
+const timeHandler = (event) => {
   const timeIn = document.querySelector('#timein');
   const timeOut = document.querySelector('#timeout');
   timeIn.value = timeOut.value = event.target.value;
-
+  event.currentTarget.removeEventListener(event.type, timeHandler);
+  event.currentTarget.addEventListener(event.type, timeHandler);
 };
 time.addEventListener('change', timeHandler);
+
+const numberRooms = document.querySelector('#room_number');
+
+const numberRoomsHandler = (event) => {
+  const capacity = document.querySelector('#capacity');
+  const item = capacity.children;
+
+  function sort(number) {
+    for (let index = 0; index < item.length; index++) {
+      item[index].value === number ? item[index].disabled = false : item[index].disabled = true;
+    }
+    capacity.value = number;
+  }
+
+  if (numberRooms.value === '1') {
+    sort('1');
+  } else if (numberRooms.value === '2') {
+    for (let index = 0; index < item.length; index++) {
+      const itemValue = item[index].value;
+      item[index].disabled = !(itemValue === '1' || itemValue === '2');
+      capacity.value = '1';
+    }
+  } else if (numberRooms.value === '3') {
+    for (let index = 0; index < item.length; index++) {
+      const itemValue = item[index].value;
+      item[index].disabled = !(itemValue === '1' || itemValue === '2' || itemValue === '3');
+      capacity.value = '1';
+    }
+  } else {
+    sort('0');
+  }
+  event.currentTarget.removeEventListener(event.type, numberRoomsHandler);
+  event.currentTarget.addEventListener(event.type, numberRoomsHandler);
+};
+numberRooms.addEventListener('change', numberRoomsHandler);
